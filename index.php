f6ccc02a5244d9fbe02a81203f2116b933dc0153
hiroshikado <okonomiyakunarahiroimono@yahoo.co.jp>
2020-10-25 14:07:16 +0900

    add-echo

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8" >
+	<title>プログラムを書いてみよう</title>
+	<link rel="stylesheet" href="/php/css/skyblue.css">
+</head>
+<body>
+	<!-- ここから -->
+	<?php 
+	// 1行だけのプログラム
+	echo "PHPで始めるプログラミング！"; 
+	?>
+	<!-- ここまで -->
+</body>
+</html>
